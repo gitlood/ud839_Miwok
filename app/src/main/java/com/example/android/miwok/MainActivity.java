22a79bbd315606c5a8fa1c9b9121c37cc9d8b683
The Lood <unilood@gmail.com>
2022-05-24 12:06:57 +1200

    Lesson 2: Arrays, Lists, Loops, & Custom Classes

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,7 +15,11 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 // import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,8 +28,61 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        //Find the View that shows the number category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+
+        //Set a clicklistener on that view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create a new intent to open the {@link Numbers Activity}
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+
+                //Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        TextView family = (TextView) findViewById(R.id.family);
+
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create a new intent to open the {@link Family Activity}
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+
+                //Start the new activity
+                startActivity(familyIntent);
+            }
+        });
+
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create a new intent to open the {@link Colors Activity}
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+
+                //Start the new activity
+                startActivity(colorsIntent);
+            }
+        });
+
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        phrases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create a new intent to open the {@link Phrases Activity}
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+
+                //Start the new activity
+                startActivity(phrasesIntent);
+            }
+        });
     }
 }
